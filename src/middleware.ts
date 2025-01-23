b5628b495ad03fa4d09c35c6761191b42535eb72
AbuRahat889 <aburahatshaum889@gmail.com>
2025-01-23 12:15:26 +0600

    set Brianaurin project

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import Cookies from "js-cookie";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
   // Redirect to login if token is not present
   if (!token) {
     const loginUrl = new URL("/login", request.url);
-    Cookies.remove("token");
-    Cookies.remove("accessToken");
+    // Cookies.remove("token");
+    // Cookies.remove("accessToken");
     return NextResponse.redirect(loginUrl);
   }
   // Proceed to the requested route
