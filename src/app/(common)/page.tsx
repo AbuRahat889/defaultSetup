99da67de81bcca4400c7b7fa895497c9024a95e0
AbuRahat889 <aburahatshaum889@gmail.com>
2025-07-06 14:36:34 +0600

    refactor: Clean up Home component by removing unnecessary whitespace

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-backgroundColor text-textColor container mx-auto">
-        this is home page{" "}
-      </h1>
+      <h1 className="bg-backgroundColor text-textColor container mx-auto">this is home page </h1>
     </div>
   );
 }
